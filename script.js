3050a5d63326d5929f280fff6cb04b01bda8e6d2
Eduardo Filho <eduardo.s.c.filho.73@gmail.com>
2026-05-21 18:39:13 +0000

    Início de fundamentos de Javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+//Isso aqui é um comentário
+
+
+
+/**
+ * Este é um 
+ * comentário de
+ * múltiplas linhas
+ */
+
+var nome;
+let data_nascimento = "2009-08-01";
+//const CPF = 00000000000;
+
+let num1 = 2;
+let num2 = 5;
+
+let soma = num1 + num2;
+
+console.log(soma);
+
+//conectivo lógico? AND OR NOT * && || !
+
+if (!nome) {
+    console.log("Não está preenchido")
+}
